a98deb4e5d951af2d3ce273232e7f0978eb81af8
Bozhidar Stoyanov <stoyanov.bojidar@gmail.com>
2025-11-24 16:48:31 +0200

    Add clean Repository (pattern) with Interface and implementation.

---
+++ LeagueApi/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeagueApi.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        IQueryable<T> GetAll();
+        Task<T> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ LeagueApi/Repositories/Repository.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using LeagueApi.Data;
+
+namespace LeagueApi.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly AppDbContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public IQueryable<T> GetAll() => _dbSet;
+
+        public async Task<T> GetByIdAsync(int id) => await _dbSet.FindAsync(id);
+
+        public async Task AddAsync(T entity)
+        {
+            _dbSet.Add(entity);
+            await Task.CompletedTask;
+        }
+
+        public void Update(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+    }
+}
