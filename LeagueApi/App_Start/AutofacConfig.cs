7b2dbb63eacf68e8a212e108ce157466b0bf7e08
Bozhidar Stoyanov <stoyanov.bojidar@gmail.com>
2025-11-24 18:38:37 +0200

    Add Autofac (Dependency Injection) config and register it in Global.cs

---
+++ LeagueApi/App_Start/AutofacConfig.cs
@@ -0,0 +1,30 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using LeagueApi.Data;
+using LeagueApi.Repositories;
+using System.Reflection;
+using System.Web.Http;
+
+namespace LeagueApi.App_Start
+{
+    public static class AutofacConfig
+    {
+        public static void Register()
+        {
+            var builder = new ContainerBuilder();
+
+            // Register API controllers
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            // Register DbContext per request
+            builder.RegisterType<AppDbContext>().InstancePerRequest();
+
+            // Repositories - UnitOfWork
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
+
+            var container = builder.Build();
+            var resolver = new AutofacWebApiDependencyResolver(container);
+            GlobalConfiguration.Configuration.DependencyResolver = resolver;
+        }
+    }
+}
