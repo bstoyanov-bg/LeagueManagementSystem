f0882be493c7513077be2cb02adf5c6d6c87955c
Bozhidar Stoyanov <stoyanov.bojidar@gmail.com>
2025-11-24 18:15:40 +0200

    Add clean Unit of work (pattern) with interface and implementation.

---
+++ LeagueApi/Repositories/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+using LeagueApi.Models;
+
+namespace LeagueApi.Repositories
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<Team> Teams { get; }
+        IRepository<Match> Matches { get; }
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ LeagueApi/Repositories/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using LeagueApi.Data;
+using LeagueApi.Models;
+
+namespace LeagueApi.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        private IRepository<Team> _teams;
+        private IRepository<Match> _matches;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IRepository<Team> Teams => _teams ?? (_teams = new Repository<Team>(_context));
+        public IRepository<Match> Matches => _matches ?? (_matches = new Repository<Match>(_context));
+
+        public async Task<int> SaveChangesAsync() => await _context.SaveChangesAsync();
+
+        public void Dispose() => _context.Dispose();
+    }
+}
