ac7e0bd2ba82e2590ef6f9d7e065adafd2127188
Bozhidar Stoyanov <stoyanov.bojidar@gmail.com>
2025-11-24 16:23:13 +0200

    Add AppDbContext and adjust the relations via fluent api.

---
+++ LeagueApi/Data/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using System.Data.Entity;
+using LeagueApi.Models;
+
+namespace LeagueApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Match> Matches { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Team>()
+                .HasMany(t => t.HomeMatches)
+                .WithRequired(m => m.HomeTeam)
+                .HasForeignKey(m => m.HomeTeamId)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Team>()
+                .HasMany(t => t.AwayMatches)
+                .WithRequired(m => m.AwayTeam)
+                .HasForeignKey(m => m.AwayTeamId)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Team>().Property(t => t.Name).IsRequired().HasMaxLength(100);
+        }
+    }
+}
