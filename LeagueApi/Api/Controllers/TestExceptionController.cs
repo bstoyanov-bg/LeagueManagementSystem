50a25c41e89aa4330588d66fa1897c58fcdfe57c
Bozhidar Stoyanov <stoyanov.bojidar@gmail.com>
2025-11-25 23:12:27 +0200

    Add Test controller for exceptions.

---
+++ LeagueApi/Api/Controllers/TestExceptionController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.Http;
+
+namespace LeagueApi.Controllers
+{
+    [RoutePrefix("api/test")]
+    public class TestExceptionController : ApiController
+    {
+        public TestExceptionController()
+        {
+
+        }
+
+        [HttpGet, Route("")]
+        public IHttpActionResult ThrowTestException()
+        {
+            throw new ArgumentNullException("Test argument null exception from controller!");
+        }
+    }
+}
